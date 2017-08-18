35bbfb6db420a4c73f37d3845eaba38d36248a00
unknown <richwbates@gmail.com>
2017-08-18 10:11:51 -0400

    configured 10 second timer to display on an h2 tag

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,27 @@
 var timerCount = 0;
-
+var timer = 10;
 
 function startTimer(){
-	timerCount = setTimeout(timesUp(){}, 10000);
+	timerCount = setInterval(countDown, 1000);
 }
 
+function countDown(){
+	timer--;
+
+	$('body').html("<h2>" + timer + "</h2>");
+	console.log(timerCount);
+
+	if(timer == 0){
+		stopTimer();
+	}
+}
 
 function stopTimer(){
-	clearTimeout(timerCount);
+	clearInterval(timerCount);
 }
 
 function timesUp(){
-	
+
 }
+
+startTimer();
